61cbbb8578363ad27a6ef25b5c94bea5110a4e45
HackedZach <zay0106@icloud.com>
2022-11-21 22:57:20 -0500

    start of index

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'static'), { redirect: false }));
 
-app.get("/", (req, res) => {
-    res.render("index", {
-        text: "Yolo!",
+app.get('/', (req, res) => {
+    res.render('index', {
+        text: 'Yolo!',
     });
 });
 
@@ -17,6 +17,5 @@ const postRouter = require('./routes/posts');
 
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
-
 
 app.listen(3000);
