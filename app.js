56f41b02f3fb1052743d5b4c441a97e6bb959b79
HackedZach <zay0106@icloud.com>
2022-11-21 20:44:38 -0500

    wef

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, 'static'), { redirect: false }));
+
 app.get("/", (req, res) => {
     res.render("index", {
         text: "Yolo!",
